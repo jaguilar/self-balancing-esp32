d3a6e2375cb6be4961384ad1ad8ff619fedabc2a
James Aguilar <aguilar.james@gmail.com>
2024-12-28 16:50:15 -0500

    Add sensor interface.

---
+++ lib/motor/src/sensor.h
@@ -0,0 +1,20 @@
+#ifndef MOTOR_SENSOR_H
+#define MOTOR_SENSOR_H
+
+namespace motor {
+
+class Sensor {
+ public:
+  virtual ~Sensor() {}
+
+  // Returns the current angle of the in decidegrees, between 0 and 3600.
+  virtual int angle() = 0;
+
+  // Returns the rate of rotation in degrees/sec during the last sensing
+  // period. Positive is clockwise.
+  virtual int rate() = 0;
+};
+
+}  // namespace motor
+
+#endif  // MOTOR_SENSOR_H
