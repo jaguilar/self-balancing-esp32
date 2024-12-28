ffe4d5bf325c5a5193c7700c9a78934b6210c5d8
James Aguilar <aguilar.james@gmail.com>
2024-12-27 22:40:15 -0500

    Make an actually constexpr intratio.

--- lib/intpid/src/intratio.h
+++ lib/intpid/src/intratio.h
@@ -1,48 +1,47 @@
 #include <cmath>
 #include <cstdint>
+#include <expected>
+#include <format>
 #include <limits>
 
 namespace intpid {
 
-struct Ratio {
-  int32_t num;
-  int32_t den_shift;
+struct IntRatio {
+  int32_t numerator;
+  int32_t log2_denominator;
 
-  constexpr Ratio(const float value, const int32_t max_input,
-                  const float max_error = 0.01f) {
-    if constexpr (value <= 0) {
-      static_assert(false, "Only positive ratios are supported");
-    }
-
-    // Calculate the largest power of two multiple of max_input that does not
-    // overflow.
-
-    int i;
-    for (constexpr int i = 0; i < 32; ++i) {
-      constexpr float max_product = value * (1 << i);
-      if (max_product >=
-          static_cast<float>(std::numeric_limits<int32_t>::max())) {
-        --i;
-        break;
-      }
-    }
-    constexpr int32_t denom_shift = i;
-    constexpr int32_t num = value * (1 << denom_shift);
-    static_assert(
-        std::abs(value - static_cast<float>(num) / (1 << denom_shift)) / value <
-            max_error,
-        "ratio is too small/large to be precisely represented");
-    return ratio;
+  friend int32_t operator*(int32_t l, IntRatio r) {
+    return l * r.numerator >> r.log2_denominator;
   }
-
-  friend int32_t operator*(int32_t l, Ratio r) {
-    return l * r.num >> r.den_shift;
-  }
-
-  // Converts a float to an IntRatio that approximates the float.
-  // max_input is the maximum input we will observe.
-  constexpr Ratio FromFloat(float value, const int32_t max_input,
-                            const float max_error = 0.01f) {}
 };
+
+constexpr std::expected<IntRatio, std::string> ToIntRatio(
+    float value, int32_t max_input, float max_error = 0.01f) [[noinline]] {
+  if (value <= 0) {
+    return std::unexpected(std::format("Value must be positive: {}\n", value));
+  }
+
+  int i;
+  for (i = 0; i < 32; ++i) {
+    float max_product = max_input * value * (1 << i);
+    if (max_product >=
+        static_cast<float>(std::numeric_limits<int32_t>::max())) {
+      --i;
+      break;
+    }
+  }
+  int32_t denom_shift = i;
+  int32_t num = value * (1 << denom_shift);
+
+  // Verify we've found a close enough ratio, or else we'll fail.
+  float calculated_ratio = static_cast<float>(num) / (1 << denom_shift);
+  float relative_error = std::abs(calculated_ratio - value) / value;
+  if (relative_error > max_error) {
+    return std::unexpected(
+        std::format("{} => ({} / (2^{})) = {} err={} max_err={}\n", value, num,
+                    denom_shift, calculated_ratio, relative_error, max_error));
+  }
+  return IntRatio{.numerator = num, .log2_denominator = denom_shift};
+}
 
 }  // namespace intpid
