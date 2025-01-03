19645f1c530f712823ff68204ced3c0371e664fb
James Aguilar <aguilar.james@gmail.com>
2025-01-02 19:50:32 -0500

    Add an interface to sensor to reset the current angle.

--- lib/motor/src/sensor.h
+++ lib/motor/src/sensor.h
@@ -13,6 +13,9 @@ class Sensor {
   // Returns the rate of rotation in degrees/sec during the last sensing
   // period. Positive is clockwise.
   virtual int rate() = 0;
+
+  // Sets the current angle. Calling this without arguments zeroes the sensor.
+  virtual void SetAngle(int angle = 0) = 0;
 };
 
 }  // namespace motor
